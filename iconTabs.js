b9ec688a2048f827d7835ff2666e999582f5404b
Jonas Naumann <98706266+JonasNaumann@users.noreply.github.com>
2022-03-15 18:48:09 +0100

    Add files via upload

---
+++ iconTabs.js
@@ -0,0 +1,13 @@
+//icon tab input links
+document.getElementById("iconTab1").onclick = function () {
+  window.open("https://instagram.com/jonaumusic");
+};
+document.getElementById("iconTab2").onclick = function () {
+  window.open("https://open.spotify.com/artist/3iQJ6BMlhaKpviuBveR0cL");
+};
+document.getElementById("iconTab3").onclick = function () {
+  window.open("https://music.apple.com/de/artist/jonau/id1475763740");
+};
+document.getElementById("iconTab4").onclick = function () {
+  window.open("https://soundcloud.com/jonaumusic");
+};
